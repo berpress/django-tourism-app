ba0b5367bdce8a39c8aa171b50721e6f495014c3
alexanderlozovoy <berpress@gmail.com>
2022-04-05 13:00:50 +0300

    Add templates

--- market_pj/urls.py
+++ market_pj/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import mainapp.views as mainapp
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", mainapp.main, name="main"),
+    path("list_of_accommodations/", include("mainapp.urls", namespace="acc")),
 ]
